a0fa96dfd22320a1ed5223d89763d6b51496fdf6
Marco Mannoni <mannoni@windowslive.com>
2021-07-01 13:41:52 +0200

    Update deployment.ps1

--- deployment.ps1
+++ deployment.ps1
@@ -6,7 +6,7 @@
     Deployment File
 
 .NOTES
-  Version:        1.0
+  Version:        3.0
   Author:         Fabio Mannoni
   Creation Date:  15.06.2021
 
@@ -14,143 +14,148 @@
 
 #>
 
-#---------------------------------------------------------[Script Parameters]------------------------------------------------------
+#---------------------------------------------------------[Script Parameter]------------------------------------------------------
 
-# Load global parameters file
+# Parameter Datei laden
 Write-Host -ForegroundColor Cyan "`t Loading configuration file"
 . "$PSScriptRoot\parameter.ps1"
 WriteSuccess "`t Config file successfully loaded"
 
-# Set Error Action to Stop
+# Verhalten bei Scriptfehlern auf "Stop" setzen
 $ErrorActionPreference = "Stop"
 
-#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+#---------------------------------------------------------[Vorbereitungen]--------------------------------------------------------
 
-# Grab Time and start Transcript
+# Datum und Uhrzeit ermitteln und Log starten
 Start-Transcript -Path "$psscriptroot\deloyment.log"
 $StartDateTime = Get-Date
 WriteInfoHighlighted "Script started at $StartDateTime"
 
-# Create local admin credentials for vms
+# Passwort für Administrator in VM abfragen
 $credential = Get-Credential -Username $locadminusername -Message "Please enter complex password for user $locadminusername"
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 #region Azure Logon
 
-# Logon to the Azure Subscription and set the right context
-WriteInfoHighlighted "`t Logon to the Azure Subscription and set the right context"
+# In Azure anmelden mit Subscription ID und Tenant ID
+WriteInfoHighlighted "`t Anmeldung in Azure wird gestartet"
 Connect-AzAccount -TenantId $paramglobal.tenantid -Subscription $paramglobal.subscriptionid
-WriteSuccess "`t Azure context sucessfully set"
+WriteSuccess "`t Anmeldung in Azure war erfolgreich!"
 
-# Get account name for later tagging of resources
+# Benutzernamen abfragen für Tags
 $azaccount = Get-AzContext
 
 #endregion
 
 
-#region network deployment
+#region Netzwerk Deployment
 
-# Deploy resource group
-WriteInfoHighlighted "`t creating resource group for VNet"
+# Ressource Gruppe für vNET deployen
+WriteInfoHighlighted ("`t Erstelle die Ressource Gruppe für das vNET " + $paramvnet.Name)
 try {
-	New-AzResourceGroup -Name $paramvnet.resourcegroup -Location $paramvnet.location -Tag @{Creator=$azaccount.account.Id; "CreationDate"=$creationDate}
+	New-AzResourceGroup -Name $paramvnet.resourcegroup -Location $paramvnet.location -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate}
 } catch {
-	WriteErrorAndExitAndExit ("`t failed to create resource group " + $paramvm.resourcegroup)
+	WriteErrorAndExitAndExit ("`t Erstellung der Ressource Gruppe " + $paramvnet.resourcegroup + " fehlgeschlagen")
 }
-WriteSuccess ("`t resource group " + $paramvm.resourcegroup + " created successfully")
+WriteSuccess ("`t Ressource Gruppe " + $paramvm.resourcegroup + " erfolgreich erstellt")
 
-# Deploy VNet
-WriteInfoHighlighted "`t creating VNet"
+# VNet deployen
+WriteInfoHighlighted ("`t Erstelle VNet " + $paramvnet.NetName)
 try {
-	$vnetwork = New-AzVirtualNetwork -Name $paramvnet.NetName -Location $paramvnet.location -ResourceGroupName $paramvnet.resourcegroup -AddressPrefix $paramvnet.netaddressprefix -Tag @{Creator=$azaccount.account.Id; "CreationDate"=$creationDate}
-} catch { #netname nicht name
-	WriteErrorAndExitAndExit ("`t failed to create VNet " + $paramvnet.Name)
-}
-WriteSuccess ("`t VNet " + $paramvnet.Name + " created successfully")
-
-# Add subnet
-WriteInfoHighlighted "`t adding subnet"
-try {
-    Add-AzVirtualNetworkSubnetConfig -name $paramvnet.privatesubnetname -VirtualNetwork $vnetwork -AddressPrefix $paramvnet.privatesubnetaddressprefix #Tag entfernt
+	$vnetwork = New-AzVirtualNetwork -Name $paramvnet.NetName -Location $paramvnet.location -ResourceGroupName $paramvnet.resourcegroup -AddressPrefix $paramvnet.netaddressprefix -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate}
 } catch {
-	WriteErrorAndExitAndExit ("`t failed to create subnet " + $paramvnet.privatesubnetname)
+	WriteErrorAndExitAndExit ("`t Erstellung des vNET " + $paramvnet.NetName + "fehlgeschlagen" )
 }
-WriteSuccess ("`t subnet " + $paramvnet.privatesubnetname + " created successfully")
+WriteSuccess ("`t vNET " + $paramvnet.NetName + " erfolgreich erstellt")
 
-# Associate subnet to VNet
-WriteInfoHighlighted "`t Associate subnet to VNet"
+# Subnet erstellen
+WriteInfoHighlighted ("`t Erstelle Subnet" + $paramvnet.privatesubnetname)
+try {
+    Add-AzVirtualNetworkSubnetConfig -name $paramvnet.privatesubnetname -VirtualNetwork $vnetwork -AddressPrefix $paramvnet.privatesubnetaddressprefix
+} catch {
+	WriteErrorAndExitAndExit ("`t Erstellung des Subnets " + $paramvnet.privatesubnetname + " fehlgeschlagen")
+}
+WriteSuccess ("`t Subnet " + $paramvnet.privatesubnetname + " erfolgreich erstellt")
+
+# Subnet dem vNET hinzufügen
+WriteInfoHighlighted "`t Füge Subnet dem vNet hinzu"
 try {
     $vnetwork | Set-AzVirtualNetwork
 } catch {
-	WriteErrorAndExitAndExit ("`t failed to associate subnet " + $paramvnet.privatesubnetname)
+	WriteErrorAndExitAndExit ("`t Subnet dem vNET hinzufügen fehlgeschlagen")
 }
-WriteSuccess ("`t subnet " + $paramvnet.privatesubnetname + " associated successfully")
+WriteSuccess ("`t Subnet erfolgreich dem vNET " + $paramvnet.NetName + " hinzugefügt")
 
 #endregion
 
-#region VM deployment
+#region VM Deployment
 
-# Deploy resource group
-WriteInfoHighlighted "`t creating resource groups"
+# Ressource Gruppe für VM deployen
+WriteInfoHighlighted ("`t Erstelle die Ressource Gruppe für die VM " + $paramvm.Name)
 try {
-	New-AzResourceGroup -Name $paramvm.resourcegroup -Location $paramvm.location -Tag @{Creator=$azaccount.account.Id; "CreationDate"=$creationDate}
+	New-AzResourceGroup -Name $paramvm.resourcegroup -Location $paramvm.location -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate}
 } catch {
-	WriteErrorAndExitAndExit ("`t failed to create resource group " + $paramvm.resourcegroup)
+	WriteErrorAndExitAndExit ("`t Erstellung der Ressource Gruppe " + $paramvm.resourcegroup + " fehlgeschlagen")
 }
-WriteSuccess ("`t resource group " + $paramvm.resourcegroup + " created successfully")
+WriteSuccess ("`t Ressource Gruppe " + $paramvm.resourcegroup + " erfolgreich erstellt")
 
-# Get network information
+# Netzwerk Informationen für VM Konfiguration abfragen
 $vnet = Get-AzVirtualNetwork -Name $paramvnet.netname -resourcegroupname $paramvnet.resourcegroup
 $subnetconfig = Get-AzVirtualNetworkSubnetConfig -Name $paramvnet.privatesubnetname -VirtualNetwork $vnet
 
-# Create Public IP Address
-$publicIp = New-AzPublicIpAddress -Name $paramvnet.publicipaddressname -ResourceGroupName $paramvm.resourcegroup -AllocationMethod $paramvnet.publicipallocation -Location $region
-
-
-# Deploy network security group
-WriteInfoHighlighted ("`t creating network security group for " + $paramvm.Name)
+# Öffentliche IP Adresse für VM erstellen
+WriteInfoHighlighted ("`t Erstelle öffentliche IP für die VM " + $paramvm.Name)
 try {
-	$nsg = New-AzNetworkSecurityGroup -Name $paramvm.nsgname -resourcegroupname $paramvm.resourcegroup -Location $paramvm.location -SecurityRules $nsgrule1 -Tag @{Creator=$azaccount.account.Id; "CreationDate"=$creationDate}
+	$publicIp = New-AzPublicIpAddress -Name $paramvnet.publicipaddressname -ResourceGroupName $paramvm.resourcegroup -AllocationMethod $paramvnet.publicipallocation -Location $region
 } catch {
-	WriteErrorAndExit ("`t failed to create network security group for " + $paramvm)
+	WriteErrorAndExitAndExit ("`t Erstellung der öffentlichen IP " + $paramvnet.publicipaddressname + " fehlgeschlagen")
 }
-WriteSuccess ("`t network security group " + $paramvm.nsgname + " created successfully")
+WriteSuccess ("`t Öffentliche IP " + $paramvnet.publicipaddressname + " erfolgreich erstellt")
 
-# Create network interface configuration
-WriteInfoHighlighted ("`t creating network interface configuration for " + $paramvm.Name)
+# NSG für VM erstellen
+WriteInfoHighlighted ("`t Erstelle die NSG für die VM " + $paramvm.Name)
+try {
+	$nsg = New-AzNetworkSecurityGroup -Name $paramvm.nsgname -resourcegroupname $paramvm.resourcegroup -Location $paramvm.location -SecurityRules $nsgrule1 -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate}
+} catch {
+	WriteErrorAndExitAndExit ("`t Erstellung der NSG " + $paramvm.nsgname + " fehlgeschlagen")
+}
+WriteSuccess ("`t NSG " + $paramvnet.nsgname + " erfolgreich erstellt")
+
+# Erstelle Konfiguration der Netzwerkkarte
+WriteInfoHighlighted ("`t Erstelle Konfiguration der Netzwerkkarte für VM " + $paramvm.Name)
 try {
     $ipconfig = New-AzNetworkInterfaceIpConfig -Name "IPConfigPrivate" -PrivateIpAddressversion IPv4 -PrivateIpAddress $paramvm.nicip -Subnetid $subnetconfig.Id -PublicIpAddressId $publicip.Id
 } catch {
-	WriteErrorAndExit ("`t failed to create network interface configuration for " + $paramvm.Name)
+	WriteErrorAndExit ("`t Erstellung der Konfiguration der Netzwerkkarte für VM " + $paramvm.Name + " fehlgeschlagen")
 }
-WriteSuccess ("`t network interface configuration for " + $paramvm.Name + " created successfully")
+WriteSuccess ("`t Konfiguration der Netzwerkkarte für VM " + $paramvnet.Name + " erfolgreich erstellt")
 
-# Deploy network interface card and assign it to the subnet
-WriteInfoHighlighted ("`t creating network interface for " + $paramvm.Name)
+# Netzwerkkarte erstellen und dem Subnet zuweisen
+WriteInfoHighlighted ("`t Erstelle Netzwerkkarte für VM " + $paramvm.Name)
 try {
 	$nic = New-AzNetworkInterface -Name $paramvm.nicname -resourcegroupname $paramvm.resourcegroup -Location $paramvm.location `
-		-NetworkSecurityGroupId $nsg.Id -IpConfiguration $ipconfig -Tag @{Creator=$azaccount.account.Id; "Creation date"=$creationDate}
+		-NetworkSecurityGroupId $nsg.Id -IpConfiguration $ipconfig -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate}
 } catch {
-	WriteErrorAndExit ("`t failed to create network interface for " + $paramvm.Name)
+	WriteErrorAndExit ("`t Erstellung der Netzwerkkarte für VM " + $paramvm.Name + " fehlgeschlagen")
 }
-WriteSuccess ("`t network interface for " + $paramvm.Name + " created successfully")
+WriteSuccess ("`t Netzwerkkarte für VM " + $paramvnet.Name + " erfolgreich erstellt")
 
-# Build VM from parameters
-$vmconfig = New-AzVMConfig -VMName $paramvm.Name -VMSize $paramvm.size -LicenseType $paramvm.licensetype
-$vmconfig = Set-AzVMOSDisk -VM $vmconfig -Name $paramvm.osdisk -Caching $paramvm.osdiskcaching -CreateOption fromImage
-$vmconfig = Set-AzVMOperatingSystem -VM $vmconfig -Windows -ComputerName $paramvm.Name -Credential $credential -ProvisionVMAgent
-$vmconfig = Set-AzVMSourceImage -VM $vmconfig -PublisherName $paramvm.publishername -Offer $paramvm.offer -Skus $paramvm.sku -Version latest
-$vmconfig = Add-AzVMNetworkInterface -VM $vmconfig -Id $nic.Id
-$vmconfig = Set-AzVMBootDiagnostic -VM $vmconfig -Enable -resourcegroupname $paramstoragediag.resourcegroup -StorageAccountName $paramstoragediag.Name
+# VM Konfiguration zusammenstellen
+$vmconfig = New-AzVMConfig -VMName $paramvm.Name -VMSize $paramvm.size -LicenseType $paramvm.licensetype					#Name, VM Grösse, Lizenzierung
+$vmconfig = Set-AzVMOSDisk -VM $vmconfig -Name $paramvm.osdisk -Caching $paramvm.osdiskcaching -CreateOption fromImage				#Diskname (C:), Cache
+$vmconfig = Set-AzVMOperatingSystem -VM $vmconfig -Windows -ComputerName $paramvm.Name -Credential $credential -ProvisionVMAgent		#Betriebssystem, Computername, Admin Account, AzureVMAgent	
+$vmconfig = Set-AzVMSourceImage -VM $vmconfig -PublisherName $paramvm.publishername -Offer $paramvm.offer -Skus $paramvm.sku -Version latest	#Betriebssystem Hersteller, Version
+$vmconfig = Add-AzVMNetworkInterface -VM $vmconfig -Id $nic.Id											#Netzwerkkarte
+$vmconfig = Set-AzVMBootDiagnostic -VM $vmconfig -Enable -resourcegroupname $paramstoragediag.resourcegroup -StorageAccountName $paramstoragediag.Name	#BootDiagnose, StorageAccount
 
-# Deploy VM
-WriteInfoHighlighted ("`t deploying vm " + $paramvm.Name)
+# VM Deployment
+WriteInfoHighlighted ("`t Erstelle VM " + $paramvm.Name)
 try {
-	New-AzVM -resourcegroupname $paramvm.resourcegroup -Location $paramvm.location -VM $vmconfig -Verbose -DisableBginfoExtension -Tag @{Creator=$azaccount.account.Id; "CreationDate"=$creationDate} 
+	New-AzVM -resourcegroupname $paramvm.resourcegroup -Location $paramvm.location -VM $vmconfig -Verbose -DisableBginfoExtension -Tag @{Ersteller=$azaccount.account.Id; "ErstellDatum"=$creationDate} 
 } catch {
-	WriteErrorAndExit ("`t failed to deploy vm " + $paramvm.Name)
+	WriteErrorAndExit ("`t Erstellung der VM " + $paramvm.Name + " fehlgeschlagen")
 }
-WriteSuccess ("`t vm " + $paramvm.Name + " deployed successfully")
+WriteSuccess ("`t VM " + $paramvm.Name + " erfolgreich erstellt")
 
 #endregion
